bae9c3e252555b44133d0025e2308141fb607c9a
danhausen <danihausen@live.com>
2021-06-02 19:11:19 -0300

    Callback_checkboxstate

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TasksTile extends StatelessWidget {
+class TasksTile extends StatefulWidget {
+  @override
+  _TasksTileState createState() => _TasksTileState();
+}
+
+class _TasksTileState extends State<TasksTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool? checkboxState) {
+    setState(() {
+      isChecked = checkboxState!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
+      title: Text('This is a task',
+          style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null,
+          )),
+      trailing: TaskCheckbox(
+        isChecked,
+        checkboxCallback,
       ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
+
+  final bool checkboxState;
+  final Function(bool?) toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
